6cc867b43e0bb07c0115a39e7e8389a78cfd0fbb
keng <odinkum2222@gmail.com>
2023-02-18 13:58:38 +0700

    advisor_report

--- application/views/additon_view/advisor_report_view.php
+++ application/views/additon_view/advisor_report_view.php
@@ -5,7 +5,7 @@
         <div class="page-header mb-3">
           <div class="d-flex justify-content-between">
             <h3 class="mt-3">รายงานการปฎิบัติงานของครูที่ปรึกษา</h3>
-            <a class="btn btn-primary align-self-end" href="#"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร </a>
+            <!-- <a class="btn btn-primary align-self-end" href="#"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร </a> -->
           </div>
         </div>
 
@@ -16,9 +16,11 @@
               <div class="row">
 
                 <div class="col-12 col-sm-7">
-                  <div class="d-flex justify-content-end">
+                  <div class="d-flex justify-content-start">
 
-                    <select class="form-select me-2" name="users_filter_user_type" onchange="document.adminForm.submit();">
+                    <p class="m-0">ภาคเรียนที่ 2 ปีการศึกษา 2565</p>
+
+                    <!-- <select class="form-select me-2" name="users_filter_user_type" onchange="document.adminForm.submit();">
                       <option value="">- ค้าหารายแผนก -</option>
                       <option value="ช่างยนต์">ช่างยนต์</option>
                       <option value="ช่างไฟฟ้ากำลัง">
@@ -37,9 +39,9 @@
                         คอมพิวเตอร์ธุรกิจ
                       </option>
                       <option value="โยธาฯ">โยธาฯ</option>
-                    </select>
+                    </select> -->
 
-                    <a class="btn btn-primary" href="/ui/4.2.3.admin-users-add.html"><i class="fa-solid fa-magnifying-glass"></i></a>
+                    <!-- <a class="btn btn-primary" href="/ui/4.2.3.admin-users-add.html"><i class="fa-solid fa-magnifying-glass"></i></a> -->
 
                   </div>
                 </div>
@@ -47,151 +49,68 @@
             </div>
             <div class="card-body">
               <div>
-                <div class="row d-none d-md-flex">
+
+                <!-- <div class="row d-none d-md-flex">
                   <div class="col-2"><strong>ห้องที่ปรึกษา</strong></div>
                   <div class="col-2"><strong>สัปดาห์ปฏิบัติทั้งหมด</strong></div>
                   <div class="col-2"><strong>สัปดาห์ที่ปฏิบัติงาน</strong></div>
                   <div class="col-3"><strong>สัปดาห์ที่ไม่ได้ปฏิบัติงาน</strong></div>
                   <div class="col-1"><strong>ร้อยละ</strong></div>
                   <div class="col-2"><strong>สุปผลปฏิบัติงาน</strong></div>
-                </div>
+                </div> -->
 
-                <hr />
-
-                <div class="row gy-2">
-                  <div class="col-12 col-md-2">
+                <div class="row gy-2 mt-3">
+                  <div class="col-12 col-md-6">
                     <div class="d-flex">
-                      <span class="d-md-none w-50">ห้องที่ปรึกษา: </span>E1
+                      <span class="d-md-none">แผนก: </span>เทคโนโลยีสารสนเทศ
                     </div>
                   </div>
-                  <div class="col-12 col-md-2">
+                  <div class="col-12 col-md-6">
                     <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ปฏิบัติทั้งหมด: <br />18:</span>
+                      <!-- <span class="d-md-none">พิมพ์เอกสาร: <br /></span> -->
                       <div>
-                        <div class="d-block">18</div>
+                        <a class="btn btn-primary align-self-end" href="#"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร </a>
                       </div>
                     </div>
                   </div>
-                  <div class="col-12 col-md-2">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ที่ปฏิบัติงาน </span>18
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-3">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ที่ไม่ได้ปฏิบัติงาน </span>0
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-1">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">ร้อยละ </span>100.00%
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-2">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สรุปผลปฏิบัติงาน </span>100.00%
-                    </div>
-                  </div>
                 </div>
-                <hr />
 
-                <div class="row gy-2">
-                  <div class="col-12 col-md-2">
+                <hr>
+
+                <div class="row gy-2 mt-3">
+                  <div class="col-12 col-md-6">
                     <div class="d-flex">
-                      <span class="d-md-none w-50">ห้องที่ปรึกษา: </span>E5
+                      <span class="d-md-none">แผนก: </span>ช่างยนต์
                     </div>
                   </div>
-                  <div class="col-12 col-md-2">
+                  <div class="col-12 col-md-6">
                     <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ปฏิบัติทั้งหมด: <br />18:</span>
+                      <!-- <span class="d-md-none">พิมพ์เอกสาร: <br /></span> -->
                       <div>
-                        <div class="d-block">18</div>
+                        <a class="btn btn-primary align-self-end" href="#"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร </a>
                       </div>
                     </div>
                   </div>
-                  <div class="col-12 col-md-2">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ที่ปฏิบัติงาน </span>18
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-3">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ที่ไม่ได้ปฏิบัติงาน </span>0
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-1">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">ร้อยละ </span>100.00%
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-2">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สรุปผลปฏิบัติงาน </span>100.00%
-                    </div>
-                  </div>
                 </div>
-                <hr />
 
-                <div class="row gy-2">
-                  <div class="col-12 col-md-2">
+                <hr>
+
+                <div class="row gy-2 mt-3">
+                  <div class="col-12 col-md-6">
                     <div class="d-flex">
-                      <span class="d-md-none w-50">ห้องที่ปรึกษา: </span>E6
+                      <span class="d-md-none">แผนก: </span>ช่างไฟฟ้ากำลัง
                     </div>
                   </div>
-                  <div class="col-12 col-md-2">
+                  <div class="col-12 col-md-6">
                     <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ปฏิบัติทั้งหมด: <br />18:</span>
+                      <!-- <span class="d-md-none">พิมพ์เอกสาร: <br /></span> -->
                       <div>
-                        <div class="d-block">18</div>
+                        <a class="btn btn-primary align-self-end" href="#"><i class="fa-solid fa-print"></i> พิมพ์เอกสาร </a>
                       </div>
                     </div>
                   </div>
-                  <div class="col-12 col-md-2">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ที่ปฏิบัติงาน </span>18
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-3">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สัปดาห์ที่ไม่ได้ปฏิบัติงาน </span>0
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-1">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">ร้อยละ </span>100.00%
-                    </div>
-                  </div>
-                  <div class="col-12 col-md-2">
-                    <div class="d-flex">
-                      <span class="d-md-none w-50">สรุปผลปฏิบัติงาน </span>100.00%
-                    </div>
-                  </div>
                 </div>
-                <hr />
 
-                <div id="pagination" class="row gy-3">
-                  <div class="col-12 col-sm-6 text-center">
-                    <span>Showing:</span>
-                    <select class="form-select d-inline" style="width: 15%">
-                      <option value="5" selected>5</option>
-                      <option value="10">10</option>
-                      <option value="10">30</option>
-                      <option value="10">50</option>
-                      <option value="10">100</option>
-                    </select>
-                    <span>of</span>
-                    <span>200</span>
-                  </div>
-                  <div class="col-12 col-sm-6 text-center">
-                    <a class="btn btn-outline-secondary" href="#">ก่อนหน้า</a>
-                    <a class="btn btn-primary" href="#">1</a>
-                    <a class="btn btn-secondary" href="#">2</a>
-                    <a class="btn btn-secondary" href="#">3</a>
-                    <a class="btn btn-secondary" href="#">4</a>
-                    <a class="btn btn-secondary" href="#">5</a>
-                    <a class="btn btn-outline-secondary" href="#">ถัดไป</a>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
